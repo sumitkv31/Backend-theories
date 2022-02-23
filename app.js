5968be3a18e5635fb48b1f0fd9248a5ed0bd5be4
sumitkv31 <sumitkv31@gmail.com>
2022-02-23 20:51:18 +0530

    backend

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //function to handle incoming request and response to the request
 function handleRequest(request, response) {
-  response.statusCode = 200;
+  response.statusCode = 200; //a sucess status code
+  response.end("<h1>Hello World!</h1>");
 }
 
 //creating our own server
 const http = require("http");
 const server = http.createServer(handleRequest);
-server.listen(1000);
+server.listen(3000);
