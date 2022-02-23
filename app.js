37b9a76e93c11c32ce12467fe89a41498d989136
sumitkv31 <sumitkv31@gmail.com>
2022-02-23 22:37:59 +0530

    backend

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
 //function to handle incoming request and response to the request
 function handleRequest(request, response) {
-  response.statusCode = 200; //a sucess status code
-  response.end("<h1>Hello World!</h1>");
+  //url is a property of request which stores the path
+  if (request.url === "/currenttime") {
+    response.statusCode = 200;
+    response.end("<h1>" + new Date().toISOString() + "</h1>");
+  } else if (request.url === "/") {
+    response.statusCode = 200; //a success status code
+    response.end("<h1>Hello World!</h1>"); //response of server after success
+  }
 }
 
 //creating our own server
